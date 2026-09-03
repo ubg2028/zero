0528d97ff4343e46f6312abf3398e9798e6e14b6
ubg2028 <308474343+ubg2028@users.noreply.github.com>
2026-09-03 20:12:19 +0530

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // उदाहरण: https://rahul.github.io
+  site: 'https://ubg2028.github.io',
+  // अगर आपका रेपो नाम 'my-astro-blog' है तो '/my-astro-blog/' लिखें।
+  // अगर रेपो नाम 'YOUR_USERNAME.github.io' है तो सिर्फ '/' लिखें।
+  base: '/zero/',
+});
